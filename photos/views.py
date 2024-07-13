c88847c58cdd9f253ff4c44440c336d94e423d8a
Miky <retiredtotuscany@gmail.com>
2024-07-13 06:29:16 -0400

    first commit

---
+++ photos/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'photos/index.html')
+
+def gallery(request):
+    return render(request, 'photos/gallery.html')
+
+def viewPhoto(request, pk):
+    return render(request, 'photos/photo.html')
+
+def addPhoto(request):
+    return render(request, 'photos/add.html')
