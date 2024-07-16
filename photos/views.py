5afcb87b0361e490f8afa5b9604640b037714560
Miky <retiredtotuscany@gmail.com>
2024-07-16 13:44:06 -0400

    Working with Bootstrap on index.html

--- photos/views.py
+++ photos/views.py
@@ -1,14 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .form import ImageForm
+from .models import Image
 
 # Create your views here.
 def index(request):
-    return render(request, 'photos/index.html')
+    return render(request, 'pages/index.html')
 
 def gallery(request):
-    return render(request, 'photos/gallery.html')
+    return render(request, 'pages/gallery.html')
 
 def viewPhoto(request, pk):
-    return render(request, 'photos/photo.html')
+    return render(request, 'pages/photo.html')
 
 def addPhoto(request):
-    return render(request, 'photos/add.html')
+    if request.method == "POST":
+        form = ImageForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            obj = form.instance
+            return render(request, "pages/add.html", {"obj": obj})
+    else:
+        form = ImageForm()
+    img = Image.objects.all()
+    return render(request, "pages/add.html", {"img": img, "form": form})
+
--- photoshare/settings.py
+++ photoshare/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = ''
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'photoshare.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,7 +80,7 @@ WSGI_APPLICATION = 'photoshare.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR + 'db.sqlite3',
     }
 }
 
@@ -119,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL="media/"
+MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
