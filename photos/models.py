b9d861b98a85ebd0fe7711a22cd781fc2749520e
Miky <retiredtotuscany@gmail.com>
2024-07-16 11:37:17 -0400

    Working with Bootstrap on index.html

--- photos/models.py
+++ photos/models.py
@@ -3,3 +3,10 @@ from django.db import models
 # Create your models here.
 class Photo(models.Model):
     file = models.FileField(upload_to="files")
+
+# Create your models here.
+class Image(models.Model):
+    caption=models.CharField(max_length=100)
+    image=models.ImageField(upload_to="img/%y")
+    def __str__(self):
+        return self.caption
